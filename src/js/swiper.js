59f1ef1e3e0b86997f78f24a144159309adb50cb
Afonso Fagundes <afonsodev21@gmail.com>
2025-08-26 17:47:35 -0300

    swiper-pagination adicionada

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const swiperClients = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 15,
-    loop: true,
-    allowTouchMove: true,  // Habilita swipe
-    pagination: true,      // Sem bullets
-    navigation: false,      // Sem setas
-    breakpoints: {
-      320: { slidesPerView: 2 },
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 5 }
-    }
-  });
+const swiperClients = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 15,
+  loop: true,
+  allowTouchMove: true,  // habilita deslizar com dedo/mouse
+  pagination: {
+    el: ".swiper-pagination", // referência à div correta
+    clickable: true           // permite clicar nas bolinhas
+  },
+  navigation: false,         // sem setas
+  breakpoints: {
+    320: { slidesPerView: 2 },
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 5 }
+  }
 });
